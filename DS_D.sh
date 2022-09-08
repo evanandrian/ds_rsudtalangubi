088efc707890bc1be7509daf664f2981a9c39c9f
efan andrian <efan.andrian05@gmail.com>
2022-09-08 14:21:49 +0700

    first commit

---
+++ DS_D.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+"D:\ds_rsudtalangubi\kill_DS.exe" $1 &
+git pull
+"D:\ds_rsudtalangubi\DesktopService.exe" $1 &
+
---
+++ DS_E.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+"E:\ds_rsudtalangubi\kill_DS.exe" $1 &
+git pull
+"E:\ds_rsudtalangubi\DesktopService.exe" $1 &
+
+
